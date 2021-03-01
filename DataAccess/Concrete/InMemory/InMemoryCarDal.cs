47f2b12c7d08bad257922ec7baba5229281d5f4d
Ömer Ekinci <omrekinci5@gmail.com>
2021-03-01 12:18:02 +0300

    build dataaccess and inmemory trains

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entitiess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> { 
+                new Car{CarId=1, BrandId=1,ColorId=1,ModelYear="2015",DailyPrice=150000,Description="araba"},
+                new Car{CarId=2, BrandId=1,ColorId=1,ModelYear="2016",DailyPrice=500000,Description="araba"},
+                new Car{CarId=3, BrandId=2,ColorId=1,ModelYear="2015",DailyPrice=150000,Description="araba"},
+                new Car{CarId=4, BrandId=2,ColorId=1,ModelYear="2014",DailyPrice=850000,Description="araba"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {    
+            // burada firstordefault da kullanılabilir.
+            Car carToDelete = _cars.SingleOrDefault(c=>c.CarId == car.CarId);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAllByCategory(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            
+        }
+    }
+}
---
+++ Entitiess/Concrete/Color.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entitiess.Concrete
+{
+    public class Color
+    {
+        public int colorId { get; set; }
+        public string colorName { get; set; }
+    }
+}
